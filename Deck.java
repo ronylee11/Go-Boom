e65a80187149d2588b1a5069274db2662dcdacfa
Yap Choo Kath Moon <1211102270@student.mmu.edu.my>
2023-05-13 12:47:55 +0800

    Co-authored-by: jl1012 <jl1012@users.noreply.github.com>

--- Deck.java
+++ Deck.java
@@ -1,16 +1,16 @@
-import java.util.ArrayList;
+import java.util.*;
 
 public class Deck {
     // Combine Suite and Character
     private ArrayList<String> deck = new ArrayList<>();
-    private char[] D_Suit = { 'c', 'h', 'd', 's' };
-    private char[] D_Rank = { 'A', '2', '3', '4', '5', '6', '7', '8', '9', 'X', 'J', 'Q', 'K' };
+    private String[] D_Suit = { "c", "h", "d", "s" };       //Change the char to String
+    private String[] D_Rank = { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
 
     Deck() {
 
         for (int i = 0; i < D_Suit.length; i++) {
             for (int j = 0; j < D_Rank.length; j++) {
-                deck.add(Character.toString(D_Suit[i]) + Character.toString(D_Rank[j]));
+                deck.add((D_Suit[i]) + (D_Rank[j]));
             }
         }
         System.out.println(deck);
