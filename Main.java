532462aaef05ceb834d27baff3978ce70d302431
Lee Rong Yi <1211311027@student.mmu.edu.my>
2023-05-16 23:07:44 +0800

    Add clearScreen helper function

--- Main.java
+++ Main.java
@@ -1,6 +1,26 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Game game = new Game();
         game.start();
+        Scanner input = new Scanner(System.in);
+        String text = input.nextLine();
+
+        clearScreen();
+        System.out.println("Game Over! " + text);
+        input.close();
+    }
+
+    public static void clearScreen() {
+        try {
+            if (System.getProperty("os.name").contains("Windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                System.out.print("\033\143");
+            }
+        } catch (IOException | InterruptedException ex) {
+        }
     }
 }
