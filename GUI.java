f1bec80c7b52ef435559ff2a8d39aa7e392aa7c6
Yap Choo Kath Moon <1211102270@student.mmu.edu.my>
2023-06-06 17:18:44 +0800

    add a new scene

--- GUI.java
+++ GUI.java
@@ -1,35 +1,18 @@
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.application.Platform;
 import javafx.stage.Stage;
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundImage;
-import javafx.scene.layout.BackgroundPosition;
-import javafx.scene.layout.BackgroundRepeat;
-import javafx.scene.layout.BackgroundSize;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.ScrollPane.ScrollBarPolicy;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.scene.text.FontPosture;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Line;
-import javafx.geometry.Pos;
-import javafx.geometry.Insets;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.input.MouseEvent;
+
 
 public class GUI extends Application{
     
@@ -38,11 +21,11 @@ public class GUI extends Application{
     }
     @Override
     public void start(Stage stage) {
-        
-        Group root = new Group();
+        //set scenes
         StackPane pane = new StackPane();
         Scene scene = new Scene(pane, 1080, 720);
-        
+
+        //set background for menu
         Image icon = new Image("Image/Go Boom Logo.png");
         Image bg = new Image("Image/Go Boom Background.png");
         stage.getIcons().add(icon);
@@ -50,7 +33,7 @@ public class GUI extends Application{
         Background bg1 = new Background(background);
         pane.setBackground(bg1);
         stage.setTitle("GO-BOOM!!");
-
+        //set text for game
         Text whitestuff = new Text();
         whitestuff.setText("MENU");
         whitestuff.setFont(Font.font("Times New Roman", FontWeight.BOLD, FontPosture.REGULAR, 90));
@@ -59,15 +42,13 @@ public class GUI extends Application{
         whitestuff.setStrokeWidth(2);
         whitestuff.setTranslateY(-250);
         pane.getChildren().add(whitestuff);
-
+        //set buttons for menu
         Button playBtn = new Button("Play");
         playBtn.setPrefSize(100, 50);
         playBtn.setTranslateY(-100);
-        playBtn.setOnAction((EventHandler<ActionEvent>) new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Play Button clicked!");
-            }
+        playBtn.setOnAction(event -> {
+            Scene scene2 = new Scene(new GameGUI(stage, scene), 1080, 720);
+            stage.setScene(scene2);
         });
         pane.getChildren().add(playBtn);
 
@@ -78,6 +59,14 @@ public class GUI extends Application{
             System.out.println("Resume Button clicked!");
         });
         pane.getChildren().add(resumeBtn);
+        
+        Button quitbtn = new Button("Quit");
+        quitbtn.setPrefSize(100, 50);
+        quitbtn.setTranslateY(100);
+        quitbtn.setOnAction(event -> {
+            Platform.exit();
+        });
+        pane.getChildren().add(quitbtn);
 
         
         stage.setScene(scene);
