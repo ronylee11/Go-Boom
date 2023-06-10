5d75f2e3fc52cbb5daf86e7fb7b8ba9ef06cf248
Yap Choo Kath Moon <1211102270@student.mmu.edu.my>
2023-06-10 22:57:13 +0800

    update GUI and make that card display based on player hand

--- GUI.java
+++ GUI.java
@@ -8,94 +8,90 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.control.Button;
 import javafx.scene.control.ContentDisplay;
 import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
-import javafx.scene.text.FontPosture;
 import javafx.scene.paint.Color;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 
-public class GUI extends Application{
-    
+public class GUI extends Application {
+
     public static void main(String[] args) {
         launch(args);
     }
+
     @Override
     public void start(Stage stage) {
-        //set scenes
+        // Set up the main menu scene
         StackPane pane = new StackPane();
         Scene scene = new Scene(pane, 1080, 720);
+        stage.setResizable(false);
 
-        //set background for menu
+        // Set background for the menu
         Image icon = new Image("Image/Go Boom Logo.png");
         Image bg = new Image("Image/Go Boom Background.png");
         stage.getIcons().add(icon);
-        BackgroundImage background = new BackgroundImage(bg, null,null,null,null);
+        BackgroundImage background = new BackgroundImage(bg, null, null, null, null);
         Background bg1 = new Background(background);
-        ImageView playImg = new ImageView(new Image("Image/bomb icon.png"));
-        ImageView resumeImg = new ImageView(new Image("Image/play icon.png"));
-        ImageView quitImg = new ImageView(new Image("Image/quit icon.png"));
 
         pane.setBackground(bg1);
         stage.setTitle("GO-BOOM!!");
 
-        //load font
-        Font font = Font.loadFont(getClass().getResourceAsStream("Font/PokerInOctoberDemo.otf"),100);
-        
-        
-        //set text for game
+        // Load font
+        Font font = Font.loadFont(getClass().getResourceAsStream("Font/PokerInOctoberDemo.otf"), 100);
+
+        // Set text for the game
         Text text = new Text();
         text.setText("MENU");
         text.setFont(font);
-
-        //text.setFill(Color.WHITE);
-        //text.setStroke(Color.BLACK);
-        //text.setStrokeWidth(2);
-
         text.setTranslateY(-200);
         pane.getChildren().add(text);
 
-        //set buttons for menu
-        Button playBtn = new Button("Play");
-        playBtn.setStyle("-fx-font-family: Arial; -fx-font-size: 14px;");
-        //playBtn.setStyle("-fx-background-color: #ff0000; ");
-        playBtn.setGraphic(playImg);
-        playBtn.setContentDisplay(ContentDisplay.LEFT);
-        
-        playBtn.setPrefSize(100, 50);
-        playBtn.setTranslateY(-100);
+        // Set buttons for the menu
+        ImageView playImg = new ImageView(new Image("Image/bomb icon.png"));
+        Button playBtn = createButton("Play", playImg);
         playBtn.setOnAction(event -> {
             Scene scene2 = new Scene(new GameGUI(stage, scene), 1080, 720);
             stage.setScene(scene2);
         });
+        playBtn.setTranslateY(-100);
         pane.getChildren().add(playBtn);
 
-        Button resumeBtn = new Button("Resume");
-        resumeBtn.setStyle("-fx-font-family: Arial; -fx-font-size: 14px;");
-        resumeBtn.setGraphic(resumeImg);
-        resumeBtn.setContentDisplay(ContentDisplay.LEFT);
-        resumeBtn.setPrefSize(150, 50);
-        resumeBtn.setTranslateY(0);
+        ImageView resumeImg = new ImageView(new Image("Image/play icon.png"));
+        Button resumeBtn = createButton("Resume", resumeImg);
         resumeBtn.setOnAction(event -> {
             System.out.println("Resume Button clicked!");
         });
         pane.getChildren().add(resumeBtn);
-        
-        Button quitBtn = new Button("Quit");
-        quitBtn.setStyle("-fx-font-family: Arial; -fx-font-size: 14px;");
-        quitBtn.setGraphic(quitImg);
-        quitBtn.setContentDisplay(ContentDisplay.LEFT);
-        quitBtn.setPrefSize(100, 50);
-        quitBtn.setTranslateY(100);
+
+        ImageView quitImg = new ImageView(new Image("Image/quit icon.png"));
+        Button quitBtn = createButton("Quit", quitImg);
         quitBtn.setOnAction(event -> {
             Platform.exit();
         });
+        quitBtn.setTranslateY(100);
         pane.getChildren().add(quitBtn);
 
-        
         stage.setScene(scene);
         stage.show();
     }
-    
+
+    private Button createButton(String buttonText, ImageView imageView) {
+        Button button = new Button(buttonText);
+        button.setStyle("-fx-font-family: Arial; -fx-font-size: 14px;");
+        button.setGraphic(imageView);
+        button.setContentDisplay(ContentDisplay.LEFT);
+        button.setPrefSize(200, 50);
+        return button;
+    }
+
+    private Scene createGameScene(Stage stage) {
+        // Create the game GUI scene here (you can customize it based on your requirements)
+        // For example:
+        StackPane gamePane = new StackPane();
+        Scene gameScene = new Scene(gamePane, 1080, 720);
+        // Add game-related elements to the gamePane
+        
+        return gameScene;
+    }
 }
--- GameGUI.java
+++ GameGUI.java
@@ -9,11 +9,16 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class GameGUI extends Pane {
+public class GameGUI extends AnchorPane {
     private ScrollPane scrollPane;
     private VBox content;
     private Pane pane2 = new Pane();
@@ -25,33 +30,46 @@ public class GameGUI extends Pane {
         this.mainMenuScene = mainMenuScene;
 
         content = new VBox(10); // Set spacing between cards
-        content.setMaxWidth(200); // Set the maximum width of the card container
-        content.setTranslateX((1080 - content.getMaxWidth()) / 2); // Center the card container horizontally
-        scrollPane = new ScrollPane(content);
-        scrollPane.setLayoutX(30);
-        scrollPane.setLayoutY(460);
-        scrollPane.setPrefSize(1000, 180);
+
+        scrollPane = new ScrollPane();
+        scrollPane.setContent(content);
         scrollPane.setFitToWidth(true);
         scrollPane.setPrefHeight(240);
-        AnchorPane.setTopAnchor(scrollPane, 0.0);
-        AnchorPane.setRightAnchor(scrollPane, 0.0);
-        AnchorPane.setBottomAnchor(scrollPane, 0.0);
-        AnchorPane.setLeftAnchor(scrollPane, 0.0);
         BackgroundFill backgroundFill = new BackgroundFill(Color.LIGHTBLUE, null, null);
         scrollPane.setBackground(new Background(backgroundFill));
 
         Button returnBtn = createButton("Return", 14, 28, 130, 60);
         Button button2 = createButton("Save", 920, 28, 130, 60);
         Button button3 = createButton("Draw", 350, 200, 130, 210);
-        Button imageButton = createImageButton("Image/sA.png",0, 0, 200, 200);
-        Button imageButton1 = createImageButton("Image/s2.png",0, 0, 200, 200);
 
-        imageButton.setLayoutX(100);
         pane2.getChildren().addAll(returnBtn, button2, button3);
-        //content.getChildren().addAll(imageButton, imageButton1);
         getChildren().addAll(scrollPane, pane2);
-        content.getChildren().add(player1Hand());
+
+        // Create player hand HBox instances
+        HBox player1HandBox = createPlayerHandBox(player1Hand);
+        HBox player2HandBox = createPlayerHandBox(player2Hand);
+        HBox player3HandBox = createPlayerHandBox(player3Hand);
+        HBox player4HandBox = createPlayerHandBox(player4Hand);
+
+        // Add player hand HBox instances to the content VBox
+        content.getChildren().addAll(player1HandBox, player2HandBox, player3HandBox, player4HandBox);
+
+        // Anchor the scrollPane to the bottom of the AnchorPane
+        AnchorPane.setBottomAnchor(scrollPane, 0.0);
+        AnchorPane.setLeftAnchor(scrollPane, 0.0);
+        AnchorPane.setRightAnchor(scrollPane, 0.0);
+
+        // Anchor the pane2 to the top-right of the AnchorPane
+        AnchorPane.setTopAnchor(pane2, 10.0);
+        AnchorPane.setRightAnchor(pane2, 10.0);
+
+        // Bind the size of the content VBox to the width of the scrollPane
+        content.prefWidthProperty().bind(scrollPane.widthProperty().subtract(20));
+
+        // Scroll to the bottom of the ScrollPane when the content changes
+        scrollPane.vvalueProperty().bind(content.heightProperty());
     }
+
 
     private Button createButton(String buttonText, double layoutX, double layoutY, double minWidth, double minHeight) {
         Button button = new Button(buttonText);
@@ -61,47 +79,41 @@ public class GameGUI extends Pane {
         button.setOnAction(event -> stage.setScene(mainMenuScene));
         return button;
     }
-    private Button createImageButton(String imagePath, double layoutX, double layoutY, double minWidth, double minHeight) {
-        Button button = new Button();
-        button.setLayoutX(layoutX);
-        button.setLayoutY(layoutY);
-        button.setPrefSize(minWidth, minHeight);
-        button.setOnAction(event -> {          
-                System.out.println("Image button clicked!");              
-        });
-
-        Image image = new Image(imagePath);
-        ImageView imageView = new ImageView(image);
-        imageView.setFitWidth(minWidth);
-        imageView.setFitHeight(minHeight);
 
 
-        button.setGraphic(imageView);
+    // for testing purposes
+    ArrayList<String> player1Hand = new ArrayList<>(Arrays.asList("hA", "sA", "h2", "sQ", "d4", "s5", "s9", "d7", "h8"));
+    ArrayList<String> player2Hand = new ArrayList<>(Arrays.asList("h2", "sA", "c2", "s3", "d4", "d5", "d6", "s7", "s8", "s9", "sX", "sJ", "sQ", "sK"));
+    ArrayList<String> player3Hand = new ArrayList<>(Arrays.asList("sA", "cA", "sK", "c3", "sJ", "sX", "sX"));
+    ArrayList<String> player4Hand = new ArrayList<>(Arrays.asList("dA", "sK", "s2", "cJ", "c4", "sQ", "s6", "d7", "dX", "sA", "cA"));
 
-        return button;
+    private static HBox createPlayerHandBox(ArrayList<String> hand) {
+        HBox hBox = new HBox(10); // Set spacing between cards
+        hBox.setAlignment(Pos.CENTER); // Center the cards horizontally within the HBox
+
+        for (String card : hand) {
+            // Create ImageView for each card
+            ImageView cardView = createCardImageView(card);
+
+            // Add cardView to the HBox
+            hBox.getChildren().add(cardView);
+        }
+
+        return hBox;
     }
 
-    HBox player1Hand() {
-        Image cardImage1 = new Image(getClass().getResourceAsStream("Image/hA.png"));
-        ImageView cardView1 = new ImageView(cardImage1);
-        cardView1.setFitHeight(100);
-        cardView1.setFitWidth(75);
-        HBox.setMargin(cardView1, new Insets(0,-20,0,20));
-        cardView1.setOnMouseClicked((event) -> {
-            System.out.println("Play hA!");
+    private static ImageView createCardImageView(String card) {
+        String imagePath = "Image/" + card + ".png";
+        Image cardImage = new Image(Main.class.getResourceAsStream(imagePath));
+        ImageView cardView = new ImageView(cardImage);
+        cardView.setFitHeight(120);
+        cardView.setFitWidth(95);
+        HBox.setMargin(cardView, new Insets(0, -20, 0, -30));
+
+        cardView.setOnMouseClicked((event) -> {
+            System.out.println("Play " + card + "!");
         });
 
-        Image cardImage2 = new Image(getClass().getResourceAsStream("Image/sA.png"));
-        ImageView cardView2 = new ImageView(cardImage2);
-        cardView2.setFitHeight(100);
-        cardView2.setFitWidth(75);
-        HBox.setMargin(cardView2, new Insets(0,-20,0,0));
-        cardView2.setOnMouseClicked((event) -> {
-            System.out.println("Play sA!");
-        });
-
-        HBox hBox = new HBox(cardView1,cardView2);
-        return hBox;
-
+        return cardView;
     }
 }
