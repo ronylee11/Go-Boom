bbbaa9015099901eecd48292636814a116ce9c58
Yap Choo Kath Moon <1211102270@student.mmu.edu.my>
2023-06-18 02:06:27 +0800

    add a scoreboard at top the gui    GameGUI.java

--- GameGUI.java
+++ GameGUI.java
@@ -10,6 +10,8 @@ import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 
 import java.util.ArrayList;
@@ -24,6 +26,7 @@ public class GameGUI extends AnchorPane {
     private ScrollPane scrollPane;
     private VBox content;
     private Pane pane2 = new Pane();
+    private Pane scoreboard = new Pane();
     private Stage stage;
     private Scene mainMenuScene;
     private List<HBox> playerHandBoxes;
@@ -32,12 +35,13 @@ public class GameGUI extends AnchorPane {
     Deck decks = new Deck();
     ArrayList<ArrayList<String>> playerCards;
     ArrayList<String> gcenter;
+    private Text[] playerScores;
 
     public GameGUI(Stage stage, Scene mainMenuScene) {
         this.stage = stage;
         this.mainMenuScene = mainMenuScene;
         this.playerHandBoxes = new ArrayList<>();
-        
+
         content = new VBox(10); // Set spacing between cards
         StackPane stackPane = new StackPane();
         scrollPane = new ScrollPane();
@@ -46,12 +50,17 @@ public class GameGUI extends AnchorPane {
         scrollPane.setPrefHeight(240);
         BackgroundFill backgroundFill = new BackgroundFill(Color.LIGHTBLUE, null, null);
         scrollPane.setBackground(new Background(backgroundFill));
-        
+
+        // Create scrollboard pane
+        scoreboard.setPrefWidth(500);
+        scoreboard.setPrefHeight(100);
+        scoreboard.setBackground(new Background(backgroundFill));
+
         Button returnBtn = createButton("Return", 14, 28, 130, 60);
         Button button2 = createButton("Save", 920, 28, 130, 60);
-        
+
         pane2.getChildren().addAll(returnBtn, button2);
-        getChildren().addAll(scrollPane, pane2);
+        getChildren().addAll(scoreboard, scrollPane, pane2);
 
         // Anchor the scrollPane to the bottom of the AnchorPane
         AnchorPane.setBottomAnchor(scrollPane, 0.0);
@@ -61,6 +70,11 @@ public class GameGUI extends AnchorPane {
         // Anchor the pane2 to the top-right of the AnchorPane
         AnchorPane.setTopAnchor(pane2, 10.0);
         AnchorPane.setRightAnchor(pane2, 10.0);
+
+        // Anchor the scoreboard to the top and center of the AnchorPane
+        AnchorPane.setTopAnchor(scoreboard, 10.0);
+        AnchorPane.setLeftAnchor(scoreboard, 200.0);
+        AnchorPane.setRightAnchor(scoreboard, 200.0);
 
         // Bind the size of the content VBox to the width of the scrollPane
         content.prefWidthProperty().bind(scrollPane.widthProperty().subtract(20));
@@ -127,6 +141,41 @@ public class GameGUI extends AnchorPane {
 
         return drawView;
     }
+    // Update the scoreboard
+    public void updateScoreboard() {
+        // Clear the scoreboard pane
+        scoreboard.getChildren().clear();
+
+        // Create and position the "Scoreboard" text
+        Text scoreLabel = new Text("Scoreboard");
+        scoreLabel.setFill(Color.BLACK);
+        scoreLabel.setFont(Font.font("Arial", FontWeight.BOLD, 42));
+        scoreLabel.setTranslateY(37);
+        scoreLabel.setTranslateX(218);
+        scoreboard.getChildren().add(scoreLabel);
+
+        // Create and position the player score texts
+        for (int i = 0; i < playerScores.length; i++) {
+            Text playerScore = playerScores[i];
+            playerScore.setFill(Color.BLACK);
+            playerScore.setFont(Font.font("Arial", FontWeight.BOLD, 24));
+            playerScore.setTranslateY(80);
+            playerScore.setTranslateX(25 + 170 * i);
+            scoreboard.getChildren().add(playerScore);
+        }
+    }
+
+    // Update the player scores
+    public void updateScores() {
+        // Update player scores in the playerScores array
+        for (int i = 0; i < playerScores.length; i++) {
+            playerScores[i].setText(String.format("Player %d: %d", i + 1, game.getPlayerScore(i)));
+        }
+
+        // Update the scoreboard display
+        updateScoreboard();
+    }
+
     // Create the initial gcenter (centerbox) with cards
     private void create_first_gcenter() {
         
@@ -230,6 +279,11 @@ public class GameGUI extends AnchorPane {
 
     // Main game loop
     public void game_loop(){
+        // Initialize playerScores array
+        playerScores = new Text[4];
+        for (int i = 0; i < playerScores.length; i++) {
+            playerScores[i] = new Text();
+        }
         game.initializeGame();
         create_first_gcenter();
         game.current_determine();
@@ -238,10 +292,12 @@ public class GameGUI extends AnchorPane {
         setupPlayerHands();
         pane2.getChildren().add(createdrawView());
         content.getChildren().add(1, showPlayerTurn(game.get_currentplayer()));
+        updateScores();
 
     }
     // Restart the game loop
     public void loop_this(){
+        updateScores();
         game.initializeGame();
         playerCards = game.getPlayerCards();
         gcenter = game.center;
