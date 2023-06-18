9787258ad8113f22723a1bde6314f10f9d54bae8
Ho Jing Lu <123185415+jl1012@users.noreply.github.com>
2023-06-18 13:30:26 +0800

    Update Game.java

--- Game.java
+++ Game.java
@@ -33,6 +33,10 @@ public class Game {
             }
         }
     }
+    
+    public int getTrickNumber() {
+        return trickNumber;
+    }
 
     public void setPlayers(Player[] loadedPlayers) {
         players = loadedPlayers;
