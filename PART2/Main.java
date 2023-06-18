bfecde8ce411e404d14e102da9e790133f2daca2
Lee Rong Yi <1211311027@student.mmu.edu.my>
2023-06-18 14:36:26 +0800

    Add error checking in Greeting Menu

--- PART2/Main.java
+++ PART2/Main.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -11,20 +12,27 @@ public class Main {
         System.out.println("3. Exit");
         System.out.print("> ");
 
-        int choice = input.nextInt();
-        Game game = new Game();
+        try {
+            int choice = input.nextInt();
+            Game game = new Game();
 
-        switch (choice) {
-            case 1:
-                System.out.println("");
-                game.start();
-                break;
-            case 2:
-                System.out.println("");
-                game.load();
-            case 3:
-                System.exit(0);
-                break;
+            switch (choice) {
+                case 1:
+                    selected = true;
+                    System.out.println("");
+                    game.start();
+                case 2:
+                    selected = true;
+                    System.out.println("");
+                    game.load();
+                case 3:
+                    selected = true;
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid input. Try again (1~3)");
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input. Try again (1~3)");
         }
         input.close();
 
